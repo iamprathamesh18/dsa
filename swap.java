b5d1ba0dbb2bd58c1cbe2a8e415f8d7950b1a032
iamprathamesh18 <121452545+iamprathamesh18@users.noreply.github.com>
2023-12-31 22:03:55 +0530

    Add files via upload

--- swap.java
+++ swap.java
@@ -1,3 +1,15 @@
 public class swap {
+    public static void main(String[] args) {
+        int a = 4;
+        int b= 5;
+
+        int temp = a;
+        a=b;
+        b=temp;
+
+
+        System.out.println(a+ "    "+b);
+
+    }
 
 }
