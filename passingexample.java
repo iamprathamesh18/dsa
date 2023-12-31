476ddacc32cd2c7d4e6f09c31a3e6f8bce34c570
iamprathamesh18 <121452545+iamprathamesh18@users.noreply.github.com>
2023-12-31 22:01:53 +0530

    Add files via upload

---
+++ passingexample.java
@@ -0,0 +1,11 @@
+public class passingexample {
+    public static void main(String[] args) {
+        String chacha = "prathamesh";
+        greeting(chacha);
+    }
+    static void greeting(String naam){
+        System.out.println(naam);
+    }
+
+
+}
---
+++ passingexample2.java
@@ -0,0 +1,11 @@
+public class passingexample2 {
+    public static void main(String[] args) {
+        String name= "mayur";
+        changename(name);
+        System.out.println(name);
+    }
+    static void changename(String naam){
+        naam="narayan t";
+
+    }
+}
