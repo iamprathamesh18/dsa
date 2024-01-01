b5fe8503f9f26eaf9a8193a7d2b53d955465cce0
iamprathamesh18 <121452545+iamprathamesh18@users.noreply.github.com>
2024-01-01 19:24:32 +0530

    Add files via upload

--- armstrongnumber.java
+++ armstrongnumber.java
@@ -1,5 +1,15 @@
 public class armstrongnumber {
     public static void main(String[] args) {
-        
+        int sum = 0;
+        int a=153;
+        while(a>0){
+        int rem = a%10;
+        rem = rem*rem*rem;
+//        System.out.println(rem);
+        sum=sum+rem;
+        a=a/10;
+
+
     }
-}
+        System.out.println(sum);
+}}
