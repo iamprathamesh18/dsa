f712c3d1d3e324fd731c2a9d7c20e15a3c573fc5
iamprathamesh18 <121452545+iamprathamesh18@users.noreply.github.com>
2024-01-10 22:47:01 +0530

    Add files via upload

---
+++ array/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+//        store list of roll no.s
+
+        int[] rollno={2,5,4,8,1,4};
+        rollno[0]=4;
+        System.out.println(rollno[0]);
+    }
+}
