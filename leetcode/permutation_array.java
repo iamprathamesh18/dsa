31d80cf1ad993356e7d2b54c70d03e252e2c8a11
Prathamesh Shinde <121452545+iamprathamesh18@users.noreply.github.com>
2024-06-22 15:16:00 +0530

    Add files via upload

---
+++ leetcode/permutation_array.java
@@ -0,0 +1,17 @@
+public class permutation_array {
+    public static void main(String[] args) {
+        int[][] accounts = {{1,5},{7,3},{1,1}};
+        maximumWealth(accounts);
+
+
+    }
+    static void maximumWealth(int[][] accounts){
+        for(int row=0;row<accounts.length;row++){
+            for(int col=0;col<accounts[row].length;col++){
+                int sum = accounts[row][col];
+            }
+        }
+
+
+    }
+}
